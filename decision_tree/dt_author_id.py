6c4b7b38d13c975ae2a5088cf49a02fd03975d4f
trstephen <tyler.stephen@gmail.com>
2015-11-01 15:11:15 -0800

    Add decision tree author classifier

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -19,13 +21,16 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
+# print len(features_train[0])
 #########################################################
 ### your code goes here ###
 
+clf = tree.DecisionTreeClassifier(min_samples_split = 40)
+
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+print("Accuracy: {acc}").format(acc = accuracy_score(pred, labels_test) )
 
 #########################################################
-
-
