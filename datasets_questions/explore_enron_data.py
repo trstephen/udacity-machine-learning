e1de13a18f44940d8c8356cc1446af3ba21f9f85
trstephen <tyler.stephen@gmail.com>
2015-11-03 20:20:43 -0800

    Add enron corpus exploration examples

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,36 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print("Number of persons: {0}").format( len(enron_data) )
+print("Number of features: {0}").format( len(enron_data["SKILLING JEFFREY K"].values()) )
+print("Number of POIs: {0}").format( 
+	sum(1 for name in enron_data.values() if name["poi"] == True) )
 
+print("James Prentice's total stock value: ${0:,d}").format(
+	enron_data["PRENTICE JAMES"]["total_stock_value"] )
+print("Wesley Colwell to POIs: {0}").format(
+	enron_data["COLWELL WESLEY"]["from_this_person_to_poi"] )
+print("Skilling exercised options: ${0:,d}").format(
+	enron_data["SKILLING JEFFREY K"]["exercised_stock_options"] )
+
+for poi in ["SKILLING JEFFREY K", "LAY KENNETH L", "FASTOW ANDREW S"]:
+	print("{poi} total payments: ${total:,d}").format(
+		poi=poi.title(), total=enron_data[poi]["total_payments"])
+
+print("Number of people with salaries: {0}").format(
+	sum(1 for name in enron_data.values() if name["salary"] != "NaN") )
+print("Number of people with email addresses: {0}").format(
+	sum(1 for name in enron_data.values() if name["email_address"] != "NaN") )
+
+countMissingTotalPayments = sum( 
+	1 for name in enron_data.values() 
+	if name["total_payments"] == "NaN")
+print("Percentage without salary: {0:0.2%}").format(
+	float(countMissingTotalPayments) / len(enron_data) )
+
+countPOIsMissingTotalPayments = sum( 
+	1 for name in enron_data.values() 
+	if name["total_payments"] == "NaN" 
+	and  name["poi"] == True )
+print("Percentage of POIs without salary: {0:0.2%}").format(
+	float(countPOIsMissingTotalPayments) / len(enron_data) )
